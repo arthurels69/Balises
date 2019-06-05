dd13a239005caaeadbbf68370ce536c47b5def65
chikungugna <thomas.rey@posteo.net>
2019-06-05 15:23:25 +0200

    Sliding calendar v1

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,8 @@
 require('../scss/app.scss');
 require('bootstrap');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+
 const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
