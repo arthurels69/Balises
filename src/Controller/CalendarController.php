5b2b950e2fc8bc99b88a9343c4237759b21786e9
chikungugna <thomas.rey@posteo.net>
2019-06-06 18:14:09 +0200

    fixtures

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use App\Service\CalendarService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,65 +14,66 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class CalendarController extends AbstractController
 {
-	/**
-	 * Displays the calendar and sends shows data
-	 * @param Request $request
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 * @throws \Exception
-	 * @Route("/calendar/",
-	 *      name="calendar_home",
-	 *     methods={"GET", "POST"})
-	 */
-    public function calendarHome(Request $request)
+    // array of Strings applied in the twig date_modify filter
+    private $calendarService;
+
+    public function __construct(CalendarService $calendarService)
     {
-		$today = new \DateTime();
-
-		// array of Strings applied in the twig date_modify filter
-		$oneMoreDay = [];
-		for ($i = 0; $i < 14; $i++) {
-			$oneMoreDay[$i] = "+$i day";
-		}
-
-		return $this->render('Calendar/calendar.html.twig', [
-			'today' => $today,
-			'oneMoreDay' => $oneMoreDay
-		]);
+        $this->calendarService = $calendarService;
     }
 
-	/**
-	 * @param Request $request
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 * @throws \Exception
-	 * @Route("/calendar/{day}", name="calendar_select_day")
-	 */
-	public function calendarSelectedDay(Request $request)
-	{
-		//Retrieves the date passed in URI.
-		$selectedDay = new \DateTime(substr($request->getUri(), -10));
+    /**
+     * Displays the calendar at first visit on Calendar Page.
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     * @Route("/calendar/",
+     *      name="calendar_home",
+     *     methods={"GET", "POST"})
+     */
+    public function calendarHome(Request $request)
+    {
+        $today = new \DateTime();
 
-		// array of Strings applied in the twig date_modify filter
-		$oneMoreDay = [];
-		for ($i = 0; $i < 14; $i++) {
-			$oneMoreDay[$i] = "+$i day";
-		}
+        // array of Strings applied in the twig date_modify filter
+        $oneMoreDay = [];
+        for ($i = 0; $i < 14; $i++) {
+            $oneMoreDay[$i] = "+$i day";
+        }
 
-		// Date transmitted by the "rechercher par date" formular
-		if ($request->request->get('picked_date')) {
-			$pickedDate = $request->request->get('picked_date');
+        return $this->render('Calendar/calendar.html.twig', [
+            'today' => $today,
+            'oneMoreDay' => $this->calendarService->addMoreDays()
+        ]);
+    }
 
-			return $this->render('Calendar/calendar.html.twig', [
-				'today' => $pickedDate,
-				'oneMoreDay' => $oneMoreDay,
-			]);
+    /**
+     * Display the calendar with the selected data as input.
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     * @Route("/calendar/{day}", name="calendar_select_day")
+     */
+    public function calendarSelectedDay(Request $request)
+    {
+        //Retrieves the date passed in URI.
+        $selectedDay = new \DateTime(substr($request->getUri(), -10));
 
-			//If a date is passed in URI (selected on the carousel calendar)
-		} elseif (!empty($selectedDay)) {
-			return $this->render('Calendar/calendar.html.twig', [
-				'today' => $selectedDay,
-				'oneMoreDay' => $oneMoreDay,
-			]);
-			//If no date at all is given (first access to the page)
-		}
-	}
+        // Date transmitted by the "rechercher par date" formular
+        if ($request->request->get('picked_date')) {
+            $pickedDate = $request->request->get('picked_date');
 
+            return $this->render('Calendar/calendar.html.twig', [
+                'today' => $pickedDate,
+                'oneMoreDay' => $this->calendarService->addMoreDays()
+            ]);
+
+            //If a date is passed in URI (selected on the carousel calendar)
+        } elseif (!empty($selectedDay)) {
+            return $this->render('Calendar/calendar.html.twig', [
+                'today' => $selectedDay,
+                'oneMoreDay' => $this->calendarService->addMoreDays()
+            ]);
+        }
+    }
 }
