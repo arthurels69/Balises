72afac8f82ccb8b70e6946134e40e92c12c3a733
BEAUHAIRE Julien <Julien_BEAUHAIRE1@MacBook-Pro-de-BEAUHAIRE-2.local>
2019-06-11 13:07:12 +0200

    mapViewer - pointers from database

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -2,18 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\TheaterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MapController extends AbstractController
 {
     /**
      * @Route("/map", name="map")
+     * @param TheaterRepository $theaterRepository
+     * @return Response
      */
-    public function map()
+    public function index(TheaterRepository $theaterRepository): Response
     {
-        return $this->render('home/map.html.twig');
+        return $this->render('home/map.html.twig', [
+            'theaters' => $theaterRepository->findAll(),
+        ]);
     }
+
+
 
     /**
      * @Route("/map2", name="map2")
