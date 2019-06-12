98eba78076d9de283ff758198a030b0fa686c2d8
Connor FM <foucauld.gaudin@gmail.com>
2019-06-12 17:30:33 +0200

    successfully fetch coordinates from a set address

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Theater;
 
 class MapController extends AbstractController
 {
@@ -17,10 +18,34 @@ class MapController extends AbstractController
     /**
      * @Route("/map2", name="map2")
      */
-    public function map2(TheaterRepository $theaterRepository)
+    public function map2(TheaterRepository $theaterRepository, Theater $theater = null)
     {
+        $theater = new Theater();
+        $street = $theater->getAddress1();
+        $zipCode = $theater->getZipCode();
+        $city = $theater->getCity();
+
+        $opts = array('http'=>array('header'=>"User-Agent: StevesCleverAddressScript 3.7.6\r\n"));
+        $context = stream_context_create($opts);
+        $address = "17 rue Delandine 69002 Lyon";
+        $json=file_get_contents(
+            'https://nominatim.openstreetmap.org/search.php?q='
+            . urlencode($address)
+            . '&format=json',
+            false,
+            $context
+        );
+
+        $obj = json_decode($json, true);
+
+        $latitude = $obj[0]['lat'];
+        $longitude = $obj[0]['lon'];
+
+
         return $this->render('home/map2.html.twig', [
             'theaters' => $theaterRepository->findAll(),
+             'lat' => $latitude,
+             'long' => $longitude
         ]);
     }
 }
