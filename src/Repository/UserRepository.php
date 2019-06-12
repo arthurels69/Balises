73e97ba1fc76e1ebc108d292a1b48cdc0b80aa10
jamal <jamelec777@hotmail.com>
2019-06-12 16:59:40 +0200

    Fixation du thead du tableau lorsqu'on scroll

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,32 +19,7 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-   /**
-    * This function return the zip code of an theater
-    *
-    * @param [type] $price
-    * @return array
-    */
-    public function findCodeBy($id): array
-{
-    $conn = $this->getEntityManager()->getConnection();
-
-    $sql = '
-        SELECT * FROM user AS us
-        JOIN theater As th
-        ON th.user_id=us.id
-        WHERE th.zip_code :id        
-        ';
-    $stmt = $conn->prepare($sql);
-    $stmt->execute(['zip_code' => $id]);
-
-    // returns an array of arrays (i.e. a raw data set)
-    return $stmt->fetchAll();
-}
-
-
-
-    // /**
+      // /**
     //  * @return User[] Returns an array of User objects
     //  */
     /*
