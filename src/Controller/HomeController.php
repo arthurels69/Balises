727538c859177de5548f6b5aa627776a0b257b41
chikungugna <thomas.rey@posteo.net>
2019-06-20 10:04:58 +0200

    structure mapadoAPI ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,27 +14,28 @@ class HomeController extends AbstractController
     public function index()
     {
 
-    	$mapadoRead = new MapadoApi('ticketing:events:read');
+        $mapadoRead = new MapadoApi('ticketing:events:read');
 
-    	dump($mapadoRead->getTicketings());
+        dump($mapadoRead->getTicketings());
 
 
-    	$mapadoWrite = new MapadoApi('ticketing:events:write');
-
-    	$body = {
-		"activityUuid": "string",
-  "timezone": "string",
-  "permanent": true,
-  "eventDateList": [
-    {
-		"startDate": "2019-06-20T07:16:56Z",
-      "endDate": "2019-06-20T07:16:56Z",
-      "saleStartDate": "2019-06-20T07:16:56Z",
-      "saleEndDate": "2019-06-20T07:16:56Z",
-      "ticketing": "string",
-      "ticketPriceList": [
+        $mapadoWrite = new MapadoApi('ticketing:events:write');
+        $body='';
+        /*
+        $body = {
+        "activityUuid": "string",
+        "timezone": "string",
+        "permanent": true,
+        "eventDateList": [
         {
-			"id": "/v1/ticket_prices/1",
+        "startDate": "2019-06-20T07:16:56Z",
+        "endDate": "2019-06-20T07:16:56Z",
+        "saleStartDate": "2019-06-20T07:16:56Z",
+        "saleEndDate": "2019-06-20T07:16:56Z",
+        "ticketing": "string",
+        "ticketPriceList": [
+        {
+            "id": "/v1/ticket_prices/1",
           "facialValue": 1000,
           "type": "reduced",
           "name": "Under 18 ticket",
@@ -43,8 +44,8 @@ class HomeController extends AbstractController
           "saleStartDate": "2019-01-01T18:00:00+0200",
           "saleEndDate": "2019-01-05T18:00:00+0200",
           "templateParameters": [
-			"string"
-		],
+            "string"
+          ],
           "eventDate": {},
           "eventDatePattern": "string",
           "tax": {},
@@ -53,66 +54,66 @@ class HomeController extends AbstractController
           "ticketStartDate": "2019-06-20T07:16:56Z",
           "ticketExpirationInterval": "string",
           "sellingDeviceList": [
-			"string"
-		],
+            "string"
+          ],
           "ticketPriceSeatGroupList": [
-			"string"
-		],
+            "string"
+          ],
           "buyable": true,
           "visible": true,
           "offerRuleList": [
-			"string"
-		],
+            "string"
+          ],
           "behaviour": "string",
           "totalStock": 100,
           "unlimitedStock": true,
           "valueIncvat": null
+          }
+        ],
+        "stockContingentList": [
+        "string"
+        ],
+        "status": "string",
+        "seatConfig": "string",
+        "seatingName": "string",
+        "eventDatePattern": "string",
+        "isReservableOnline": true,
+        "totalStock": 0,
+        "unlimitedStock": true
         }
-      ],
-      "stockContingentList": [
-		"string"
-	],
-      "status": "string",
-      "seatConfig": "string",
-      "seatingName": "string",
-      "eventDatePattern": "string",
-      "isReservableOnline": true,
-      "totalStock": 0,
-      "unlimitedStock": true
-    }
-  ],
-  "eventDatePatternList": [
-			"string"
-		],
-  "wallet": "string",
-  "isOnSale": true,
-  "isOnSaleDemoMode": true,
-  "title": "string",
-  "slug": "string",
-  "activitySlug": "string",
-  "description": "string",
-  "place": "string",
-  "address": "string",
-  "city": "string",
-  "citySlug": "string",
-  "countryCode": "string",
-  "imageList": [
-			"string"
-		],
-  "schedule": "string",
-  "position": 0,
-  "customerRequiredFields": [
-			"string"
-		],
-  "templateParameters": [
-			"string"
-		],
-  "orderNotificationEmailList": [
-			"string"
-		]
-}
-
-    	$mapadoWrite->createTicketing('/v1/ticketings', $body);
+        ],
+        "eventDatePatternList": [
+            "string"
+        ],
+        "wallet": "string",
+        "isOnSale": true,
+        "isOnSaleDemoMode": true,
+        "title": "string",
+        "slug": "string",
+        "activitySlug": "string",
+        "description": "string",
+        "place": "string",
+        "address": "string",
+        "city": "string",
+        "citySlug": "string",
+        "countryCode": "string",
+        "imageList": [
+            "string"
+        ],
+        "schedule": "string",
+        "position": 0,
+        "customerRequiredFields": [
+            "string"
+        ],
+        "templateParameters": [
+            "string"
+        ],
+        "orderNotificationEmailList": [
+            "string"
+        ]
+        }
+*/
+        $mapadoWrite->createTicketing('/v1/ticketings', $body);
 
 
 
