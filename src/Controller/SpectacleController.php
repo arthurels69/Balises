9a4fb57c1b1f11e72525ff25e4ad016308d38e04
Nathalie Cortès <natyv225@gmail.com>
2019-06-25 12:33:12 +0200

    modification routes sur visu Theatre, selon Admin ou theater

--- src/Controller/SpectacleController.php
+++ src/Controller/SpectacleController.php
@@ -109,6 +109,19 @@ class SpectacleController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            /** @var UploadedFile $file */
+            $fileImage = $request->files->get('spectacle')['image'];
+            if ($fileImage) {
+                $fileName = md5(uniqid()) . '.' . $fileImage->guessExtension();
+                try {
+                    $fileImage->move($this->getParameter('logo_directory'), $fileName);
+                } catch (FileException $e) {
+                    throw new FileException($e);
+                }
+                $spectacle->setImage($fileName);
+            }
+
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('spectacle_index', [
