390212244095c4032cb0c9502d8c088ef99afd13
jamal <jamelec777@hotmail.com>
2019-06-26 10:05:49 +0200

    Mise en forme edition théâtre  terminé

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -72,7 +72,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('Email incorrect.');
         }
 
         return $user;
@@ -80,7 +80,15 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+        $validPassword=$this->passwordEncoder->isPasswordValid($user, $credentials['password']);
+        
+        if (!$validPassword) {
+            // fail authentication with a custom error
+            throw new CustomUserMessageAuthenticationException('Mot de passe incorrect.');
+        }
+
+
+        return $validPassword;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
