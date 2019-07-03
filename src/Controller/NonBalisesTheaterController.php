3b61584565c01883fd94136f1e18c577d4b813e0
ConnorFM <foucauld.gaudin@gmail.com>
2019-07-03 17:06:58 +0200

    route nonBalises secured

--- src/Controller/NonBalisesTheaterController.php
+++ src/Controller/NonBalisesTheaterController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/non/balises/theater")
+ * @Route("/nonbalisestheater")
  */
 class NonBalisesTheaterController extends AbstractController
 {
