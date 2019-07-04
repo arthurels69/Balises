33348609bf559c84e680276965c919324397c8f1
Kev <Kevin_Heitz_student2019@wilder.school>
2019-07-04 16:04:30 +0200

    merge with dev

--- src/Migrations/Version20190625092143.php
+++ src/Migrations/Version20190625092143.php
@@ -20,7 +20,9 @@ final class Version20190625092143 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection
+                ->getDatabasePlatform()
+                ->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE spectacle CHANGE image image VARCHAR(255) DEFAULT NULL');
     }
@@ -28,7 +30,9 @@ final class Version20190625092143 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf($this->connection
+                ->getDatabasePlatform()
+                ->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE spectacle CHANGE image image VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
     }
